5096608659af79746ee604c195881942607b88fc
Fernando Huaman <mfernando12@hotmail.com>
2022-10-06 22:36:29 -0500

    feat: Primera subida de proyecto

---
+++ src/main/java/com/qds/backend/evaluacion/springrestqdsnotas/SpringRestQdsNotasApplication.java
@@ -0,0 +1,13 @@
+package com.qds.backend.evaluacion.springrestqdsnotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestQdsNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestQdsNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qds/backend/evaluacion/springrestqdsnotas/SpringRestQdsNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qds.backend.evaluacion.springrestqdsnotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestQdsNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
